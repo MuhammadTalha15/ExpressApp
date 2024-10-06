a5ed193941efc3db5f32396b99e1725f215fcb5f
MuhammadTalha15 <talhaz5518@gmail.com>
2024-10-06 22:29:13 +0500

    N

--- api/main.js
+++ api/main.js
@@ -3,14 +3,14 @@ import express from 'express'
 const port = 3000;
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('../public'));
 
 app.get('/', (req, res) => {
     res.sendFile('index');
 });
 
 app.get('/about', (req, res) => {
-    res.send('This is ABout testing Route');
+    res.send('This is About testing Route');
 });
 
 app.listen(port, () => console.log(`App listening on Port ${port}`))
