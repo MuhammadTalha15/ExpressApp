36da9f8ed9bc1e2803c506287cb11a72ffed2262
Muhammad Talha <141347189+MuhammadTalha15@users.noreply.github.com>
2024-11-10 21:52:27 +0500

    Add files via upload

--- api/main.js
+++ api/main.js
@@ -1,12 +1,24 @@
 import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
 
 const port = 3000;
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static('../public'));
 
+console.log(__dirname)
+console.log(__filename)
+
 app.get('/', (req, res) => {
-    res.sendFile('index');
+    res.sendFile(path.join('/home/muhammad_talha/Software/ExpressApp/', 'public' , 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join('/home/muhammad_talha/Software/ExpressApp/', 'public' , 'about.html'));
 });
 
 app.get('/about', (req, res) => {
